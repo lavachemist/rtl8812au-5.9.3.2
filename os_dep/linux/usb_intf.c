184aba91960c47c0ded2300e2ac393b867c96f84
lavachemist <48248311+lavachemist@users.noreply.github.com>
2021-06-14 18:04:15 -0400

    Add T2U Nano

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -184,6 +184,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
   {USB_DEVICE(0x2357, 0x0103), .driver_info = RTL8812}, /* TP-Link - Archer T4UH */
   {USB_DEVICE(0x2357, 0x010D), .driver_info = RTL8812}, /* TP-Link - Archer T4U AC1300 */
   {USB_DEVICE(0x2357, 0x010E), .driver_info = RTL8812}, /* TP-Link - Archer T4UH AC1300 */
+  {USB_DEVICE(0x2357, 0x011E), .driver_info = RTL8812}, /* TP-Link - Archer T2U Nano */
   {USB_DEVICE(0x2357, 0x010F), .driver_info = RTL8812}, /* TP-Link - T4UHP */
   {USB_DEVICE(0x2357, 0x0122), .driver_info = RTL8812}, /* TP-Link - Archer T4UHP(US) v1 AC1300 */
   {USB_DEVICE(0x2604, 0x0012), .driver_info = RTL8812}, /* Tenda - U12 */
